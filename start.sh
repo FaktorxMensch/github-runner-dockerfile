cdcc3077e88af321168f4b2f83bc3401e6b4fa4a
najajan <jan@najajan.de>
2024-03-20 13:20:51 +0100

    add unattended flag to config.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: a
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
+./config.sh --unattended --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
